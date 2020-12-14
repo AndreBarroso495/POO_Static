01333264654fb40737bbdb22d012b4b9f610fdfe
AndreBarroso495 <73665987+AndreBarroso495@users.noreply.github.com>
2020-12-14 09:46:32 -0300

    Projeto concluído

---
+++ Conversor.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace POO_Static
+{
+    public class Conversor
+    {
+        private static float cotacaoDolar = 5.07f;
+
+        public static float realParaDolar(float valorRS){
+            return valorRS / cotacaoDolar;
+        }
+
+        public static float dolarParaReais(float valorUS){
+            return cotacaoDolar * valorUS;
+        }
+
+        internal static float realParaEuro()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static float cotacaoEuro = 6.12f;
+
+        public static float realParaEuro(float valorRS){
+            return valorRS / cotacaoEuro;
+        }
+
+        public static float euroParaReais(float valorEUR){
+            return cotacaoEuro * valorEUR;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POO_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( Conversor.dolarParaReais(10) );
+
+            Console.WriteLine( Conversor.realParaDolar(100) );
+
+            Console.WriteLine( Conversor.realParaEuro(100) );
+
+            Console.WriteLine( Conversor.euroParaReais(10) );
+        }
+    }
+}
